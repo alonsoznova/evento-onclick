12e3c859885525af2f09661baceead03f498e54b
alonsoznova <alonsoznova@gmail.com>
2023-09-15 00:45:25 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+function logOut(elemento){
+    elemento.innerText = "Logout";
+}
+function removeElement(elemento){
+    elemento.remove();
+}
+function mensaje(){
+    var msg = "Ninja was liked";
+    return msg
+}
